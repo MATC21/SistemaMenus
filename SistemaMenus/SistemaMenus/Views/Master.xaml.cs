128a7dcf5c1f429a10764e784467d223c355657a
MATC21 <ma.ona@itq.edu.ec>
2024-06-05 18:18:44 -0500

    Add project files.

---
+++ SistemaMenus/SistemaMenus/Views/Master.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SistemaMenus.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : ContentPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnInicio_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Inicio());
+        }
+
+        private async void btnImagenes_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Imagenes());
+        }
+    }
+}
